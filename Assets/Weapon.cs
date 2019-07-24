c106d5d45c30cb3acc09a79e0e19d936388a7391
Albert <grandia3@hotmail.com>
2019-07-24 22:23:35 +0700

    added hit effects

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
-    [SerializeField] float range;
+    [SerializeField] float range = 100f;
     [SerializeField] float damage = 10f;
-
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject impactHit;
     
     void Update()
     {
@@ -20,14 +21,33 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         // this will be the object that we hit
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
+            //effect on enemy
+            CreateHitImpact(hit);
             EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
-            if (enemyHealth == null) { return;  }
+            if (enemyHealth == null) { return; }
             enemyHealth.TakeDamage(damage);
         }
-        
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(impactHit, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.1f);
     }
 }
