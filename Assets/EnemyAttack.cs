57a9da920f212231262f18ffd9ea271925cabf28
Albert <grandia3@hotmail.com>
2019-07-29 21:53:16 +0700

    added animation event

---
+++ Assets/EnemyAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float damage = 40f;
+    
+    public void AttackHitEvent()
+    {
+        if (target == null) { return; }
+        Debug.Log("Bang");
+    }
+}
